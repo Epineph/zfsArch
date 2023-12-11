6c0839ec9e7dd0733c0542f7d585bf9f785c4e97
Epineph <44166929+Epineph@users.noreply.github.com>
2023-12-11 20:46:56 +0100

    Update test.py

--- test.py
+++ test.py
@@ -24,7 +24,8 @@ def run_command(command):
 
 def create_bootable_usb():
     if len(sys.argv) > 1:
-        iso_path = str(f for f in os.listdir(path) if f.endswith('.iso')]).strip()
+        iso_path = [f for f in os.listdir(path) if f.endswith('.iso')]
+        iso_path = str(iso_path).strip()
     else:
         iso_path = input("Enter the path to the ISO file: ")
     print("Hello, World!")
